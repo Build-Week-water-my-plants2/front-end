3ac5775b8ffa402b8684d8826b70de6da13be2e6
Arison13 <arisonarias@gmail.com>
2021-10-18 17:32:26 -0400

    fixied a couple of small bugs and made it so that the home link is accesible and when u push the add plant button it should send u to the form, just kind of setting things up

--- watermyplant/src/App.js
+++ watermyplant/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/HomePage';
 
 function App() {
   return (
     <Router>
       <div className = "App">
         <h1>Welcome to WaterMyPlant 2.0</h1>
+        <Link to='/'> Home </Link>
+
+        <Route exact path = '/' component={Home}/>
         <Route exact path = "/login" components = {Login} />
         <Route exact path = "/signup" components = {Signup} />
       </div>

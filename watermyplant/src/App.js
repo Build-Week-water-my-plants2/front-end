3612a91a7346b6e124de6a98405366bf5fe4b75f
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-18 14:09:50 -0400

    Rendered a title

--- watermyplant/src/App.js
+++ watermyplant/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className = "App">
+        <h1>Welcome to WaterMyPlant 2.0</h1>
+        <Route exact path = "/login" components = {Login} />
+        <Route exact path = "/signup" components = {Signup} />
+      </div>
+    </Router>
   );
 }
 

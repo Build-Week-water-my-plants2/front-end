66474570a433ed1eba26fb45010327795f4967b2
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-18 13:04:55 -0700

    add login.js

--- watermyplant/src/App.js
+++ watermyplant/src/App.js
@@ -1,20 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
+  
   return (
     <Router>
       <div className = "App">
-        <h1>Welcome to WaterMyPlant 2.0</h1>
-        <Route exact path = "/login" components = {Login} />
-        <Route exact path = "/signup" components = {Signup} />
-      </div>
-    </Router>
+        <nav>
+          <h1 className='site-header'>Welcome to WaterMyPlant 2.0</h1>
+          <div className='nav-links'>          
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>        
+        <Route exact path = "/login" >
+          <Login  
+            // values={formValues}
+            // update={updateForm}
+            // submit={submitForm}
+            // errorText={errorText}
+          />  
+        </Route>
+        <Route exact path = "/signup" components = {Signup} />        
+      </div>  
+    </Router>    
   );
 }
 

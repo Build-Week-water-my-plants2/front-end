59a5a80eb04282f74ae8ed05f731ff1c03ec54d2
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-19 13:44:17 -0700

    update constants.js to shaow up and down arrows which for password page, index.js yypos

--- watermyplant/src/components/data/constants.js
+++ watermyplant/src/components/data/constants.js
@@ -1,7 +1,15 @@
+// DRY code to set unival URL and API const
 const BASE_URL = 'https://api.nasa.gov/planetary/apod'
 const API_KEY = 'j42aHvMnebEBy4GuDh324XlLuEKHd8N9fg5YDM5a'
 
+// set up special label  ▼ and ▲ - ref : guided module 2.1.3
+const open = '\u25bc'
+const close = '\u25b2'
+
 export default {
     BASE_URL: BASE_URL,
-    API_KEY: API_KEY
+    API_KEY: API_KEY,
+    open: open,
+    close: close
 }
+
